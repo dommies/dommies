92aeb6135fbebc1a0e1bdfb4571dc47271a52857
Dommies <87873359+dommies@users.noreply.github.com>
2021-07-26 22:37:56 +0800

    Updated Dommies.sol
    Added price change function for pre-sale possibilities

--- contracts/Dommies.sol
+++ contracts/Dommies.sol
@@ -7,11 +7,11 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract Dommies is ERC721Enumerable, Ownable {
     uint public constant MAX_TOKENS = 10000;
-    uint public constant TOKEN_PRICE = 69 * 10 ** 15; // 0.069 ETH
-    uint public constant MAX_DOMMIES_PURCHASE = 69;
+    uint public constant MAX_DOMMIES_PURCHASE = 20;
+    uint public mintPrice = 69 * 10 ** 15; // 0.069 ETH    
     bool public isSalesActive = false;
 	string _baseTokenURI;
-    string _contractURI;	
+    string _contractURI;
 
     //LFG
     constructor(string memory baseURI) ERC721("Dommies", "DOMMIES")  {
@@ -19,15 +19,16 @@ contract Dommies is ERC721Enumerable, Ownable {
     }
 
     function mintMyNFT(address _to, uint _count) public payable {
+        uint256 totalTokens = totalSupply();
         require(isSalesActive, "Dommies Sale has not started!");
-        require(totalSupply() + _count <= MAX_TOKENS, "Sorry you tried to mint too many Dommies!");
-        require(totalSupply() < MAX_TOKENS, "No more Dommies left!");
-        require(_count <= MAX_DOMMIES_PURCHASE, "Leave some Dommies for others!");
-        require(msg.value >= (TOKEN_PRICE * _count), "Need more ETH to unwrap the Dommies!");
+        require((totalTokens + _count) < (MAX_TOKENS + 1), "Sorry you tried to mint too many Dommies!");
+        require(totalTokens < MAX_TOKENS, "No more Dommies left!");
+        require(_count < (MAX_DOMMIES_PURCHASE + 1), "Leave some Dommies for others!");
+        require(msg.value >= (mintPrice * _count), "Need more ETH to unwrap the Dommies!");
 
         uint mintTime = block.timestamp;
         for(uint i = 0; i < _count; i++){
-            uint mintIndex = totalSupply();
+            uint256 mintIndex = totalSupply();
             _safeMint(_to, mintIndex);
             emit TokenMinted(msg.sender, mintIndex, mintTime);
         }
@@ -42,11 +43,15 @@ contract Dommies is ERC721Enumerable, Ownable {
     }
 
     // Events
-    event TokenMinted(address owner, uint tokenId, uint mintTime);
+    event TokenMinted(address owner, uint256 tokenID, uint mintTime);
 
     // onlyOwner Functions //
     function setSaleState(bool newState) public onlyOwner {
         isSalesActive = newState;
+    }
+
+    function setMintPrice(uint newMintPrice) public onlyOwner {
+        mintPrice = newMintPrice;
     }
 
     function setBaseURI(string memory baseURI) public onlyOwner {
